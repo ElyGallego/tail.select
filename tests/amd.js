f401cea3c403040bbe68216e6d4c1c2e77c37924
Lenivyy <lenivyy@pytes.net>
2020-12-08 08:41:06 +0100

    [TASK] Add amd JS

---
+++ tests/amd.js
@@ -0,0 +1,9 @@
+
+require.config({
+    baseUrl: "../dist/js"
+})
+require(["rat.select"], function(select) {
+    require(["langs/de_AT", "plugins/ajax"], function() {
+        select("select");
+    });
+});
